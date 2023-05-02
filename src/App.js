6f041896082b9773e4627ee9e8e920f0a1a7ca16
NuranNajafli <nnajafli2021@ada.edu.az>
2023-05-02 12:48:37 +0400

    random Number Between Interval

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [min, setMin] = useState("");
+  const [max, setMax] = useState("");
+  const [random, setRandom] = useState(13);
+
+  const handleRandom = () => {
+    setRandom(Math.floor(Math.random() * (max - min + 1)) + min);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <div className="box">
+        <div className="top">
+          {" "}
+          Random Number: <span>{random}</span>
+        </div>
+        <div className="center">
+          <div className="left">
+            <label>Min</label>
+            <input
+              type="number"
+              value={min}
+              onChange={(e) => setMin(+e.target.value)}
+            />
+          </div>
+          <div className="right">
+            <label>Max</label>
+            <input
+              type="number"
+              value={max}
+              onChange={(e) => setMax(+e.target.value)}
+            />
+          </div>
+        </div>
+        <div className="bottom">
+          <button onClick={handleRandom}>Get Random Number</button>
+        </div>
+      </div>
     </div>
   );
 }
